42d654ff247972fb5a9ad6606eb8aaa57b1942da
石松岩.前端架构组 <songyan_shi@163.com>
2023-07-14 11:37:04 +0800

    feat: 优化

--- popup.js
+++ popup.js
@@ -1,27 +1,48 @@
 const input = document.getElementById('npm-name-input')
 const search = document.getElementById('search-btn')
+const history = document.getElementById('history')
+const searchText = document.getElementById('search-text')
 const listContainer = document.getElementById('list-container')
 
+const NPM_URL = 'https://proxy.clickapaas.com/api/npm-search'
+const CACHE_NAME = 'npm-links-name-histroy'
+const ELEMENT_DATA_NAME = 'npm-name'
+const MAX_HISTORY_LENGTH = 20
+const ERROR_CLASS_NAME = 'is-error'
+const SUCCESS_CLASS_NAME = 'is-success'
+
+/** 记录是否处于请求状态 */
+let loading = false
 
 /** 进行搜索 */
 const doSearch = async () => {
-  const npmName = input.value
+  if (loading) return
+  const npmName = input?.value?.replace(/\s/g, '')
   if (!npmName) {
-    input.setAttribute('placeholder', '请输入依赖包名称')
+    input.setAttribute('placeholder', 'Please enter npm package name')
     return
   }
-  const list = await fetch(`https://proxy.clickapaas.com/api/npm-search?name=${npmName}`)
+
+  loading = true
+  searchText.classList.add('loading')
+  const list = await fetch(`${NPM_URL}?name=${npmName}`)
     .then(res => res.json())
+    .finally(() => {
+      loading = false
+      searchText.classList.remove('loading')
+    })
     .catch(err => {})
 
   if (Array.isArray(list)) {
     renderList(list)
+    handleCache(npmName, renderHistory)
     handleSuccessStyle()
     return
   }
   handleErrorStyle()
 }
 
+/** 渲染查询出的数据项 */
 const renderList = list => {
   const getItemHtml = item => {
     const {
@@ -74,19 +95,62 @@ const renderList = list => {
 
 /** 查询失败的处理 */
 const handleErrorStyle = () => {
-  input.classList.add('is-error')
-  search.classList.add('is-error')
+  input.classList.add(ERROR_CLASS_NAME)
+  search.classList.add(ERROR_CLASS_NAME)
 }
 
 /** 查询成功的处理 */
 const handleSuccessStyle = () => {
-  input.classList.add('is-success')
-  search.classList.add('is-success')
+  input.classList.add(SUCCESS_CLASS_NAME)
+  search.classList.add(SUCCESS_CLASS_NAME)
 }
+
+/** 缓存查询的包名 */
+const handleCache = (name, callback) => {
+  // 从chrome.storage获取数据
+  chrome.storage.local.get([CACHE_NAME], result => {
+    const data = (Array.isArray(result[CACHE_NAME]) ? result[CACHE_NAME]: [])
+      .filter(item => item !== name)
+    
+    data.unshift(name)
+    
+    if (data.length > MAX_HISTORY_LENGTH) {
+      data.length = MAX_HISTORY_LENGTH
+    }
+    // 保存数据到chrome.storage
+    chrome.storage.local.set({[CACHE_NAME]: data}, () => {
+      callback()
+    });
+  });
+}
+
+const renderHistory = () => {
+  chrome.storage.local.get([CACHE_NAME], result => {
+    const data = Array.isArray(result[CACHE_NAME]) ? result[CACHE_NAME]: []
+    const html = data.reduce((html, item) => {
+      return html += 
+        `
+          <span class="history-item nes-badge">
+            <span class="is-gray" ${ELEMENT_DATA_NAME}=${item}>${item}</span>
+          </span>
+        `
+    }, '')
+    history.innerHTML = html
+  });
+}
+
+renderHistory()
 
 search.addEventListener('click', doSearch)
 input.addEventListener('keypress', e => {
   if (e.key == 'Enter') {
     doSearch()
   }
+})
+
+history.addEventListener('click', e => {
+  const name = e.target.getAttribute(ELEMENT_DATA_NAME)
+  if (!name) return
+  input.value = name
+  doSearch()
 })
